0dadff948faed4a6c9fecec115198d50f02be9de
abenagiel <abenagiel@gmail.com>
2013-03-07 01:57:45 +0700

    abenagiel-mode first push

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Sat Mar 2 04:45:40 WIT 2013"
+#define LINUX_COMPILE_TIME "04:45:40"
+#define LINUX_COMPILE_BY "abenagiel"
+#define LINUX_COMPILE_HOST "abenagiel-mode-x"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
