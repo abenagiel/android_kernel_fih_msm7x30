f0332de9005820f032c8db18c4a5d26525d219db
abenagiel <abenagiel@abenagiel-dev.(none)>
2013-04-12 11:23:59 +0700

    adding cleanchace and many other changes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sat Mar 2 04:45:40 WIT 2013"
-#define LINUX_COMPILE_TIME "04:45:40"
+#define UTS_VERSION "#4 PREEMPT Fri Mar 8 15:41:31 WIT 2013"
+#define LINUX_COMPILE_TIME "15:41:31"
 #define LINUX_COMPILE_BY "abenagiel"
-#define LINUX_COMPILE_HOST "abenagiel-mode-x"
+#define LINUX_COMPILE_HOST "abenagiel-dev"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
